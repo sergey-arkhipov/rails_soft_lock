fb9e2b4f83be9383f73d33e45db6f7744cca6e17
George S. <lordsynergymail@gmail.com>
2025-04-29 18:41:44 +0300

    Fix config for model

--- lib/rails_soft_lock/configuration.rb
+++ lib/rails_soft_lock/configuration.rb
@@ -2,14 +2,10 @@
 
 # lib/rails_soft_lock/configuration.rb
 module RailsSoftLock
-  # Configuration class for RailsSoftLock gem.
+  # Configuration for global settings like adapter and user class.
   class Configuration
-    # List of supported lock storage adapters
-    VALID_ADAPTERS          = %i[redis nats memcached].freeze
-    # Default class used as the "locker" (usually a User model)
-    DEFAULT_LOCKED_BY_CLASS = "User"
-    # Default options for acts_as_locked_by DSL
-    DEFAULT_LOCKED_OPTIONS  = { by: :lock_attribute, scope: -> { "none" } }.freeze
+    # Supported storage adapters.
+    VALID_ADAPTERS = %i[redis nats memcached].freeze
 
     attr_reader :adapter
     # :reek:Attribute
@@ -17,52 +13,23 @@ module RailsSoftLock
     # :reek:Attribute
     attr_writer :locked_by_class
 
-    # Initializes configuration with default values.
     def initialize
-      @adapter                = :redis # Default adapter
-      @adapter_options      ||= RedisConfig.default_adapter_options # Default adapter options
-      @locked_by_class        = DEFAULT_LOCKED_BY_CLASS
-      @acts_as_locked_options = DEFAULT_LOCKED_OPTIONS.dup
+      @adapter = :redis
+      @adapter_options ||= RedisConfig.default_adapter_options
+      @locked_by_class = locked_by_class || "User"
     end
 
-    # DSL to define the attribute used as lock key and the scoping logic.
-    #
-    # @param attribute [Symbol] the attribute name to lock by (e.g., :id, :article_code)
-    # @param scope [Proc] a lambda that returns a scoping key (e.g., tenant_id)
-    # @return [void]
-    def acts_as_locked_by(attribute = nil, scope: nil)
-      options = {}
-      options[:by] = attribute if attribute
-      options[:scope] = scope if scope
-
-      @acts_as_locked_options = DEFAULT_LOCKED_OPTIONS.merge(options)
-    end
-
-    # Returns the attribute used to identify lock ownership.
-    #
-    # @return [Symbol]
-    def acts_as_locked_attribute
-      @acts_as_locked_options&.[](:by)
-    end
-
-    # Returns the evaluated scope value, typically a tenant ID or similar.
-    #
-    # @return [Object] result of the scope lambda or "none"
-    def acts_as_locked_scope
-      @acts_as_locked_options&.[](:scope)&.call
-    end
-
-    # Returns the class used to represent the locker (usually a User model).
+    # Returns the locker class constant (e.g., User).
     #
     # @return [Class]
     def locked_by_class
       @locked_by_class.is_a?(String) ? @locked_by_class.constantize : @locked_by_class
     end
 
-    # Sets the adapter (e.g., :redis, :nats, :memcached).
+    # Sets the adapter for lock storage.
     #
     # @param value [Symbol] one of the VALID_ADAPTERS
-    # @raise [ArgumentError] if adapter is not valid
+    # @raise [ArgumentError] if adapter is invalid
     # @return [void]
     def adapter=(value)
       raise ArgumentError, "Adapter must be one of: #{VALID_ADAPTERS.join(", ")}" unless VALID_ADAPTERS.include?(value)
--- lib/rails_soft_lock/model_extensions.rb
+++ lib/rails_soft_lock/model_extensions.rb
@@ -1,74 +1,108 @@
 # frozen_string_literal: true
 
 # lib/rails_soft_lock/model_extensions.rb
-
 module RailsSoftLock
   # Extend model and give methods from gem
   module ModelExtensions
     extend ActiveSupport::Concern
+
+    included do
+      class_attribute :locked_attribute, default: :id
+      class_attribute :lock_scope_proc, default: -> { "none" }
+
+      delegate :object_name, to: :class
+    end
+
     class_methods do
-      # :reek:UtilityFunction
+      # Defines which attribute to use for locking and an optional scoping block.
+      #
+      # @param attribute [Symbol] the attribute used for locking (default: :id)
+      # @param scope [Proc] a proc returning the scoping value (default: -> { "none" })
+      # @return [void]
       def acts_as_locked_by(attribute = nil, scope: nil)
-        RailsSoftLock.configuration.acts_as_locked_by(attribute, scope: scope)
+        self.locked_attribute = attribute if attribute
+        self.lock_scope_proc  = scope if scope
       end
 
+      # Fetches all locks for the model.
+      #
+      # @return [Hash]
       def all_locks
         RailsSoftLock::LockObject.new(object_name: object_name).all_locks
       end
 
+      # Unlocks a specific object key.
+      #
+      # @param object_key [String, Integer]
+      # @return [Boolean]
       def unlock(object_key)
         RailsSoftLock::LockObject.new(object_name: object_name, object_key: object_key).unlock
       end
 
+      # Returns the composed object name (model + scope).
+      #
+      # @return [String]
       def object_name
-        scope = RailsSoftLock.configuration.acts_as_locked_scope
-        "#{name}::#{scope}"
+        "#{name}::#{lock_scope_proc&.call}"
       end
     end
 
-    included do # rubocop:disable Metrics/BlockLength
-      delegate :object_name, to: :class
+    # Attempts to acquire a lock for the given user.
+    #
+    # @param user [User]
+    # @return [Hash]
+    def lock_or_find(user)
+      lock_object_for(user).lock_or_find
+    end
 
-      def lock_or_find(user)
-        lock_object_for(user).lock_or_find
-      end
+    # Unlocks the object for the given user.
+    #
+    # @param user [User]
+    # @return [Boolean]
+    def unlock(user)
+      lock_object_for(user).unlock
+    end
 
-      def unlock(user)
-        lock_object_for(user).unlock
-      end
+    # Checks if the object is locked.
+    #
+    # @return [Boolean]
+    def locked?
+      locked_by.present?
+    end
 
-      def locked?
-        locked_by.present?
-      end
+    # Returns the user who locked the object, if any.
+    #
+    # @return [User, nil]
+    def locked_by
+      user_id = base_lock_object.locked_by&.to_i
+      user_id ? RailsSoftLock.configuration.locked_by_class.find_by(id: user_id) : nil
+    end
 
-      def locked_by
-        user_id = base_lock_object.locked_by&.to_i
-        user_id ? RailsSoftLock.configuration.locked_by_class.find_by(id: user_id) : nil
-      end
+    # Returns the lock attribute value for the instance.
+    #
+    # @return [Object]
+    def object_key
+      attribute = self.class.locked_attribute
+      raise ArgumentError, "No locked attribute defined" if attribute == :none
 
-      def object_key
-        attribute = RailsSoftLock.configuration.acts_as_locked_attribute
-        raise ArgumentError, "No locked attribute defined" if attribute == :none
+      send(attribute)
+    end
 
-        send(attribute)
-      end
+    private
 
-      private
+    def lock_object_for(user)
+      RailsSoftLock::LockObject.new(
+        object_name: object_name,
+        object_key: object_key,
+        object_value: user.id.to_s
+      )
+    end
 
-      def lock_object_for(user)
-        RailsSoftLock::LockObject.new(
-          object_name: object_name,
-          object_key: object_key,
-          object_value: user.id.to_s
-        )
-      end
-
-      def base_lock_object
-        RailsSoftLock::LockObject.new(
-          object_name: object_name,
-          object_key: object_key
-        )
-      end
+    def base_lock_object
+      RailsSoftLock::LockObject.new(
+        object_name: object_name,
+        object_key: object_key
+      )
     end
   end
 end
